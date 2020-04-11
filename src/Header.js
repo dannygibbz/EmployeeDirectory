4c5eaf53d29568966b580fc61ced4c4df39aa529
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-04-11 15:48:36 -0400

    Added code for Employee Directory

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+  return (
+    <div id="header">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Employee Directory</h1>
+          <p className="lead">Searching for your employees -- the easy way!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
